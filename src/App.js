0bae2ebbcf14a8c15ce31d7bde4c46cbb72f8d0a
PutraAdam08 <putraadam322@gmail.com>
2023-10-20 16:26:18 +0700

    Add registration.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
+import { Registration } from './page/Registration';
 
 function App() {
+
+  const [currentForm, setCurrentFrom] = useState('registration')
+
   return (
     <div className="App">
-      Teset
+      <Registration/>
     </div>
   );
 }
