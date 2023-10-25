7615cff2864cbe708349e297b3c1d1ce25e627f1
PutraAdam08 <putraadam322@gmail.com>
2023-10-25 17:08:33 +0700

    AMpun udah bang capekkk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Register from './Register';
 import BasicNavbar from './component/navbar';
 import Homepage from './pages/Homepage';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Recipes from './pages/Recipe';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -18,17 +19,19 @@ const toggleForm = (formname) => {
       {
         <BrowserRouter>
           <Routes>
-            <Route path = '/' element={Login}></Route>
-            <Route path = '/Register' element={Register}></Route>
+            <Route exact path="/" element={<Login/>}>
+            </Route>
+            <Route path="/register" element={<Register/>}>
+            </Route>
+            <Route path="/homepage" element={<Homepage/>}>
+            </Route>
           </Routes>
         </BrowserRouter>
-
-        
       }
     </div>
   );
 }
-      /*currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/> */
+      /*currentForm === "login" ? <Login onFormRoutes={toggleForm} /> : <Register onFormRoutes={toggleForm}/> */
 
       /**/
 export default App;
